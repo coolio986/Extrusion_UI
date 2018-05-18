2cf2f4f2ab1cac412a787815eb6a7c8b34aae1c7
coolio986 <coolio986@hotmail.com>
2018-05-18 14:38:00 -0500

    Added plotting data from Real time and historical data

--- Digital_Indicator.Module.Display/ViewModels/DiameterViewModel.cs
+++ Digital_Indicator.Module.Display/ViewModels/DiameterViewModel.cs
@@ -1,9 +1,13 @@
 ﻿using Digital_Indicator.Logic.SerialCommunications;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
 using Prism.Commands;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,8 +17,17 @@ namespace Digital_Indicator.Module.Display.ViewModels
     public class DiameterViewModel : BindableBase
     {
         private ISerialService _serialService;
+        public DelegateCommand ResetGraph { get; set; }
 
         public ObservableCollection<string> SerialPortList { get; }
+
+        private IList<DataPoint> DiameterPoints { get; set; }
+
+        private IList<DataPoint> DiameterReference { get; set; }
+
+        public PlotModel RealTimeModel { get; private set; }
+
+        public PlotModel HistoricalModel { get; private set; }
 
         private string diameter;
         public string Diameter
@@ -22,34 +35,92 @@ namespace Digital_Indicator.Module.Display.ViewModels
             get { return diameter; }
             set { SetProperty(ref diameter, value); }
         }
-        private string _message;
-        public string Message
-        {
-            get { return _message; }
-            set { SetProperty(ref _message, value); }
-        }
 
         public DiameterViewModel(ISerialService serialService)
         {
-            Message = "View A from your Prism Module";
-
             _serialService = serialService;
             _serialService.DiameterChanged += _serialService_DiameterChanged;
 
             SerialPortList = new ObservableCollection<string>(_serialService.GetSerialPortList());
+            DiameterPoints = new List<DataPoint>();
+            DiameterReference = new List<DataPoint>();
 
-            if (_serialService.IsSimulationModeActive())
-            {
-                //************TEST DATA**********//
-                _serialService.ConnectToSerialPort("COM3");
-                //*******************************//
-            }
+
+            SetupRealTimeView();
+            SetupHistoricalView();
+
+            ResetGraph = new DelegateCommand(ResetGraph_Click);
+
+            //************TEST DATA**********//
+            _serialService.ConnectToSerialPort("COM3");
+            //*******************************//
+
+        }
+
+        private void ResetGraph_Click()
+        {
+            HistoricalModel.ResetAllAxes();
+        }
+
+        private void SetupRealTimeView()
+        {
+            var lineSeriesDiameterData = new LineSeries();
+            lineSeriesDiameterData.ItemsSource = DiameterPoints;
+
+            this.RealTimeModel = new PlotModel { Title = "Realtime Diameter" };
+            this.RealTimeModel.Series.Add(lineSeriesDiameterData);
+
+            RealTimeModel.Axes.Add(new DateTimeAxis { Position = AxisPosition.Bottom, StringFormat = "hh:mm:ss" });
+            RealTimeModel.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Minimum = .5,  });
             
+            this.RealTimeModel.Series.Add(GetNominalDiameter());
+        }
+
+        private void SetupHistoricalView()
+        {
+            var lineSeriesDiameterData = new LineSeries();
+            lineSeriesDiameterData.ItemsSource = DiameterPoints;
+
+            this.HistoricalModel = new PlotModel { Title = "Historical Diameter" };
+            this.HistoricalModel.Series.Add(lineSeriesDiameterData);
+
+            HistoricalModel.Axes.Add(new DateTimeAxis { Position = AxisPosition.Bottom, StringFormat = "hh:mm:ss" });
+            HistoricalModel.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Minimum = .5, });
+
+            this.HistoricalModel.Series.Add(GetNominalDiameter());
+        }
+
+        private LineSeries GetNominalDiameter()
+        {
+            var lineSeriesNominal = new LineSeries();
+            lineSeriesNominal.Title = "1.75";
+            lineSeriesNominal.StrokeThickness = 1;
+            lineSeriesNominal.ItemsSource = DiameterReference;
+
+            return lineSeriesNominal;
         }
 
         private void _serialService_DiameterChanged(object sender, EventArgs e)
         {
+            UpdateRealTimePlot();
+
             Diameter = sender.ToString();
+        }
+
+        private void UpdateRealTimePlot()
+        {
+            if (RealTimeModel != null)
+            {
+                this.DiameterPoints.Add(new DataPoint(DateTimeAxis.ToDouble(DateTime.Now), Convert.ToDouble(Diameter)));
+                this.DiameterReference.Add(new DataPoint(DateTimeAxis.ToDouble(DateTime.Now), 1.75));
+
+                if (RealTimeModel.Axes.Count > 1)
+                {
+                    RealTimeModel.Axes[0].Zoom(DateTimeAxis.ToDouble(DateTime.Now.AddMilliseconds(-4000)), DateTimeAxis.ToDouble(DateTime.Now.AddMilliseconds(200)));
+                    RealTimeModel.InvalidatePlot(true);
+                    HistoricalModel.InvalidatePlot(true);
+                }
+            }
         }
     }
 }
