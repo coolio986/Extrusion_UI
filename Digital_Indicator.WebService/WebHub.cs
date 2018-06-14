63b9a860c31a8c803ec9e05dc30d578fa957a814
coolio986 <coolio986@hotmail.com>
2018-06-14 11:51:47 -0500

    changed hub methods for ping pong comms

--- Digital_Indicator.WebService/WebHub.cs
+++ Digital_Indicator.WebService/WebHub.cs
@@ -19,42 +19,56 @@ namespace Digital_Indicator.Logic.WebService
         IHubContext hubContext;
         bool transmissionInProgress = false;
 
+        public override Task OnConnected()
+        {
+            //string name = Context.User.Identity.Name;
+            var test = Context.ConnectionId;
+
+            //_connections.Add(name, Context.ConnectionId);
+
+            return base.OnConnected();
+        }
+
+
+
         public WebHub()
         {
             _filamentService = WebService.GetFilamentService();
-            _filamentService.DiameterChanged += _filamentService_DiameterChanged;
+            //_filamentService.DiameterChanged += _filamentService_DiameterChanged;
 
             hubContext = GlobalHost.ConnectionManager.GetHubContext<WebHub>();
         }
 
-        private void _filamentService_DiameterChanged(object sender, EventArgs e)
-        {
-            if (!transmissionInProgress)
-            {
-                transmissionInProgress = true;
-                Task.Factory.StartNew(() =>
-                {
-                //run async so main thread isn't blocked, also keep capturing if there is a communication error
-                try
-                    {
-                        hubContext.Clients.All.ReceiveData(_filamentService.FilamentServiceVariables.ToList());
-                        Thread.Sleep(50);
-                        transmissionInProgress = false;
-                    }
-                    catch (Exception oe)
-                    {
-                        transmissionInProgress = false;
-                    }
-                });
-            }
-        }
+        //private void _filamentService_DiameterChanged(object sender, EventArgs e)
+        //{
+        //    if (!transmissionInProgress)
+        //    {
+        //        transmissionInProgress = true;
+        //        Task.Factory.StartNew(() =>
+        //        {
+        //        //run async so main thread isn't blocked, also keep capturing if there is a communication error
+        //        try
+        //            {
+        //                hubContext.Clients.All.ReceiveData(_filamentService.FilamentServiceVariables.ToList());
+        //                Thread.Sleep(50);
+        //                transmissionInProgress = false;
+        //            }
+        //            catch (Exception oe)
+        //            {
+        //                transmissionInProgress = false;
+        //            }
+        //        });
+        //    }
+        //}
 
-        public void Send(object obj)
+        public void Send(string connectionId, object obj)
         {
             // _filamentService = WebService.GetFilamentService();
 
-            //Clients.All.ReceiveData(_filamentService.FilamentServiceVariables["ActualDiameter"]);
-            //Clients.All.ReceiveData("blah");
+            //Clients.All.ReceiveData(_filamentService.FilamentServiceVariables.ToList());
+            //Clients.User(userId).RecieveData(_filamentService.FilamentServiceVariables.ToList());
+            Clients.Client(connectionId).RecieveData(_filamentService.FilamentServiceVariables.ToList());
+            
             //return message;
         }
 
