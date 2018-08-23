54356a9adc68dd05b80b20859bb5e04ac50b60a9
coolio986 <coolio986@hotmail.com>
2018-08-23 11:51:18 -0500

    Change title and assembly information

--- Digital_Indicator.Startup/Properties/AssemblyInfo.cs
+++ Digital_Indicator.Startup/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Digital_Indicator")]
+[assembly: AssemblyTitle("Filalogger - Filament Diameter Measurement")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Digital_Indicator")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("Filalogger - Filament Diameter Measurement")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
